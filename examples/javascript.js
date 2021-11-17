6a48caeea05af6b9cb00297dd1d4a91270883500
Nawaf <nawafalqari13@gmail.com>
2021-11-17 11:44:00 +0300

    Examples

--- examples/javascript.js
+++ examples/javascript.js
@@ -1,10 +1,1 @@
-const { exec } = require('child_process');
 
-exec('python C:\\Users\\nawaf\\Desktop\\azkar_api\\examples\\get_zekr.py', function (err, stdout, stderr) {
-  console.log(stdout);
-  console.log(stdout)
-  console.log(typeof stdout)
-  var zkr = JSON.parse(stdout)
-  console.log(zkr)
-  console.log(typeof zkr)
-});
