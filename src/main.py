e64be60d3ae15bc80637f46eab5251040fcfecb3
nawafalqari <nawafalqari13@gmail.com>
2022-10-17 08:43:17 +0300

    Added source files

---
+++ src/main.py
@@ -0,0 +1,55 @@
+from json import dumps
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse, PlainTextResponse, FileResponse
+from uvicorn import run
+
+from azkar import get_zekr
+from azkar import file_data as azkar_data
+
+from quran import get
+from quran import file_data as quran_data
+
+app = FastAPI(title="Azkar API", description="API للأذكار")
+
+@app.get("/")
+def index():
+   return RedirectResponse("https://github.com/nawafalqari/azkar-api")
+
+@app.get("/zekr")
+def zekr_route(request: Request):
+   return PlainTextResponse(get_zekr(request.query_params._dict))
+
+@app.get("/azkar.json")
+def azkar_file():
+   return PlainTextResponse(dumps(azkar_data, indent=3, ensure_ascii=False))
+
+@app.get("/azkar-minified.json")
+def azkar_file_minified():
+   return PlainTextResponse(dumps(azkar_data, ensure_ascii=False))
+
+@app.get("/quran")
+def quran_route(sura:str=None, jozz:int=None, page:int=None):
+   return PlainTextResponse(get(sura, jozz, page))
+
+@app.get("/quran/{page}")
+def quran_page(page:int):
+   path = f"data/quran/{page}.jpg"
+   try:
+      with open(path):
+         pass
+
+      return FileResponse(f"data/quran/{page}.jpg")
+   except:
+      return {"err": True, "message": "invalid page number"}
+
+
+@app.get("/quran.json")
+def quran_file():
+   return PlainTextResponse(dumps(quran_data, indent=3, ensure_ascii=False))
+
+@app.get("/quran-minified.json")
+def quran_file_minified():
+   return PlainTextResponse(dumps(quran_data, ensure_ascii=False))
+
+if __name__ == "__main__":
+   run("main:app", host="0.0.0.0", workers=1)
---
+++ src/quran.py
@@ -0,0 +1,82 @@
+from json import load, dumps
+from arabic_reshaper import ArabicReshaper
+
+def load_data(filename: str):
+   with open(filename, "r", encoding="utf-8") as file:
+      return load(file)
+
+file_data = load_data("data/quran.json")
+reshaper = ArabicReshaper({"delete_harakat": True})
+
+def get(sura, jozz, page):
+   data = None
+   if sura:
+      data = get_sura(sura)
+   elif jozz:
+      data = get_jozz(jozz)
+   elif page:
+      data = get_page(page)
+   else:
+      data = get_sura_data(sura, jozz, page)
+
+   return dumps(data, ensure_ascii=False)
+
+def get_sura(sura:str):
+   sura = find_sura(sura)
+   if not sura:
+      return {"err": True, "message": "Invalid sura name"}
+   sura = get_sura_data(sura)
+   if sura.get("err"):
+      return sura
+
+   sura["img"] = f"https://azkar-api.nawafhq.repl.co/quran/{sura['page']}"
+   return sura
+
+def get_jozz(jozz:int):
+   jozz = get_sura_data(jozz=jozz)
+   if jozz.get("err"):
+      return jozz
+   
+   jozz["img"] = f"https://azkar-api.nawafhq.repl.co/quran/{jozz['page']}"
+   return jozz
+
+def get_page(page:int):
+   page = get_sura_data(page=page)
+   if page.get("err"):
+      return page
+   
+   page["img"] = f"https://azkar-api.nawafhq.repl.co/quran/{page['page']}"
+   return page
+
+def get_suras_names():
+   suras_names = []
+   for aya in file_data:
+      name = aya["sura_name_ar"]
+      if name in suras_names:
+         continue
+      suras_names.append(name)
+
+   return suras_names
+
+def find_sura(sura:str):
+   suras = get_suras_names()
+   reshaped_sura = reshaper.reshape(sura)
+   for s in suras:
+      if reshaper.reshape(s) == reshaped_sura:
+         return s
+
+def get_sura_data(sura:str=None, jozz:int=None, page:int=None):
+
+   for aya in file_data:
+      aya_name = aya["sura_name_ar"]
+      aya_jozz = aya["jozz"]
+      aya_page = aya["page"]
+
+      if sura == aya_name:
+         return aya
+      if jozz == aya_jozz:
+         return aya
+      if page == aya_page:
+         return aya
+
+   return {"err": True, "message": "Invalid parameter: (sura) or (jozz) or (page)"}
